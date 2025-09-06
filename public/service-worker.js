247ee1740fbae9740e7a985ef101e14bdcc7b660
Domenico Leoncino <leoncino65domenico@gmail.com>
2025-09-06 18:25:10 +0200

    versine con icone personali

---
+++ public/service-worker.js
@@ -0,0 +1,7 @@
+// Questo file è necessario per rendere l'app installabile.
+// Per ora può rimanere vuoto, la sua presenza è ciò che conta.
+
+self.addEventListener('fetch', (event) => {
+  // Per il momento non aggiungiamo funzionalità offline.
+  // Il service worker serve solo a soddisfare i requisiti di una PWA.
+});
