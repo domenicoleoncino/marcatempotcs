ddc18ad037a958787d1186a6303bccc6298539a9
Domenico Leoncino <leoncino65domenico@gmail.com>
2025-09-08 22:03:53 +0200

    logo

--- src/components/CompanyLogo.js
+++ src/components/CompanyLogo.js
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 
-const LOGO_URL = 'https://i.imgur.com/x07K0fN.png';
+const LOGO_URL = 'https://imgur.com/gallery/tcs-IKx9HAc#tfoGjSD.pig';
 const PLACEHOLDER_URL = 'https://placehold.co/200x60/cccccc/ffffff?text=Logo';
 
 /**
